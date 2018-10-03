0f554e84664feba3ccf4e8defb15323eb1cc3b7e
James Lee <uptownhr@gmail.com>
2018-10-03 02:05:42 -0700

    testing split

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Page Component</h1>
+    <h1>Page Component exp</h1>
     <ul>
       <li><nuxt-link to="/">Home</nuxt-link></li>
       <li><nuxt-link to="/about/">About</nuxt-link></li>
